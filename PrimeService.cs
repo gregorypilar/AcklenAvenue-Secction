5c8fc99cf36f95c8d4da39c38368f65cbcbe72c6
gregorypilar <gregoryg3@gmail.com>
2020-11-05 19:42:07 +0000

    changes for dotnet app`

---
+++ PrimeService.cs
@@ -0,0 +1,23 @@
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
